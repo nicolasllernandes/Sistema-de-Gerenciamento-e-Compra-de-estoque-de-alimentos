8bde5737fc3a86b5d711d04c51aa2fa5ccf6da08
Ítalo Kohlrausch <italokohlrausch@gmail.com>
2026-09-24 17:56:11 -0300

    add alguns ex de como ficariam as opções p1

--- CodigoFonteSistemaProt.c
+++ CodigoFonteSistemaProt.c
@@ -5,9 +5,14 @@
 int main(int argc, char *argv[]) {
 	
 	char escolhamenuprincipal;
-	int escolhagce, escolhagf, escolhari;
+	int escolhagce, escolhagf, escolhari, escolhaes;
 	
-	printf("Selecione a opcao que deseja realiar: \n1 - Gestao de Controle de Estoque \n2 - Gestao de Fornecedores \n3 - Relatorios e Indicadores\n\n");
+	int cntrlvalidadedetalhes;
+	
+	char fornecedornome[100];
+	double fornecedorcnpj;
+	
+	printf("Selecione a opcao que deseja realizar: \n1 - Gestao de Controle de Estoque \n2 - Gestao de Fornecedores \n3 - Relatorios e Indicadores\n");
 	scanf("%c", &escolhamenuprincipal);
 
 	switch(escolhamenuprincipal){
@@ -16,9 +21,31 @@ int main(int argc, char *argv[]) {
 			printf("1 - Entradas / Saidas \n2 - Quantidade em Estoque \n3 - Controle de Validade \n");
 			scanf("%d", &escolhagce);
 			
-			if (escolhagce == 1 ) {printf("ENTRADAS / SAIDAS");}
-			else if (escolhagce == 2) {printf("QUANTIDADE EM ESTOQUE");}
-			else if (escolhagce == 3) {printf("CONTROLE DE VALIDADE");}	 
+			//1.1
+			if (escolhagce == 1 ){
+				printf("\nENTRADAS / SAIDAS");
+				
+			}
+			
+			
+			//1.2
+			else if (escolhagce == 2){
+				printf("\nQUANTIDADE EM ESTOQUE\n");
+				printf("\n PAO - 30\n AGUA - 50\n ARROZ - 10\n FEIJAO - 8");
+			}
+			
+			//1.3
+			else if (escolhagce == 3) {
+				
+				printf("\nCONTROLE DE VALIDADE\n\n ITENS PARA VENCER NOS PROX\n 15 DIAS: 10\n 20 DIAS: 12\n 25 DIAS: 20\n 30 DIAS: 35");
+				printf("Ver detalhes? (1: sim  0: nao)");
+				scanf("%d", &cntrlvalidadedetalhes);
+				if (cntrlvalidadedetalhes == 1){
+					printf("aqui exibira uma lista mais detalhada dos prox vencimentos, com a qntd exata de dias");
+				}
+					
+			}	 
+			
 			break;	
 		
 			
@@ -27,8 +54,26 @@ int main(int argc, char *argv[]) {
 			printf("1 - Cadastro de Fornecedores \n2 - Cotacao de Precos \n");
 			scanf("%d", &escolhagf);
 			
-			if (escolhagf == 1) {printf("CADASTRO DE FORNECEDORES");}
-			else if (escolhagf == 2) {printf("COTACAO DE PRECOS");}
+			
+			//2.1
+			if (escolhagf == 1){
+				
+				printf("\nCADASTRO DE FORNECEDORES\n");
+				printf("Insira o nome do fornecedor: ");
+				scanf(" %s", &fornecedornome);
+				
+				printf("Insira o cpnj do fornecedor: ");
+				scanf(" %d", &fornecedorcnpj);
+			
+				printf("\nFORNECEDOR CADASTRADO: %s ", fornecedornome);
+				printf("\nCNPJ DO FORNECEDOR CADASTRADO: %d", fornecedorcnpj);
+			}
+			
+			//2.2
+			else if (escolhagf == 2){
+				printf("\nCOTACAO DE PRECOS\n");
+			} 
+			
 			break;
 		
 		
@@ -37,9 +82,20 @@ int main(int argc, char *argv[]) {
 			printf("1 - Custo de mercadorias \n2 - Desperdicio de Alimentos \n");
 			scanf("%d", &escolhari);
 			
-			if (escolhari == 1) {printf("CUSTO DE MERCADORIAS");}
-			else if (escolhari == 2) {printf("DESPERDICIO DE ALIMENTOS");}
+			
+			//3.1
+			if (escolhari == 1){
+				printf("\nCUSTO DE MERCADORIAS(30 dias)\n");	
+			}
+			
+			//3.2
+			else if (escolhari == 2){
+				printf("\nDESPERDICIO DE ALIMENTOS(30 dias)\n");
+				printf("\n ALIMENTOS DESCARTADOS POR FALTA DE VALIDADE: 10\n ALIMENTOS DESCARTADOS POR USO INADEQUADO: 5");
+			}
 			break;
+			
+			
 			
 	}
 	
